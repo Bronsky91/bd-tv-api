11350bcb48ff007f565c4eb6d4b23f4e11294049
Bryan Reed <bryan87reed@gmail.com>
2022-11-02 08:23:04 -0700

    CORS support

--- index.js
+++ index.js
@@ -32,6 +32,19 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  next();
+});
+
 app.use(bodyParser.json());
 app.use("/api", routes);
 
